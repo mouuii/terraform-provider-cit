c8c4b700fe3d22a7c008143a97d8a242713671df
mouuii <49775493+mouuii@users.noreply.github.com>
2023-12-06 18:05:20 +0800

    data_resource example coffee

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 		// provider address is used in these tutorials in conjunction with a
 		// specific Terraform CLI configuration for manual development testing
 		// of this provider.
-		Address: "registry.terraform.io/cit/cit",
+		Address: "hashicorp.com/edu/hashicups",
 		Debug:   debug,
 	}
 
